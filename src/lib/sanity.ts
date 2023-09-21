fd5b2f54af50aeaa3a17938f22ab64a5ca626b2e
Dan Chambers <danlechambre@icloud.com>
2023-09-21 23:10:51 +0100

    Add Sanity client

---
+++ src/lib/sanity.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@sanity/client";
+
+export const client = createClient({
+  projectId: "your-project-id",
+  dataset: "your-dataset-name",
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
+  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+});
+
+export async function getOrganization() {
+  const org = await client.fetch('*[_type == "organization"]');
+  return org;
+}
