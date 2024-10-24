265923bcf5c0b86cb576ebf3cf5f4922995b3e58
Dan Chambers <danlechambre@icloud.com>
2024-10-24 22:47:31 +0100

    Add required config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import clerk from "@clerk/astro";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({ applyBaseStyles: false }), clerk()],
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "passthrough",
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
