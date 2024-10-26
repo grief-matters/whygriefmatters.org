ace334a3644af853f050c83bcf8e3e39bd05a985
Dan Chambers <danlechambre@icloud.com>
2024-10-26 17:21:25 +0100

    Migrate to 'hybrid' rendering mode with Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false })],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+    imageService: "passthrough",
+  }),
+  output: "hybrid",
 });
