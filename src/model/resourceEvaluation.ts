767091b133fb18eb238c372968613e53fb2ded7e
Dan Chambers <danlechambre@icloud.com>
2024-11-09 19:41:41 +0000

    Implement model and query for new API

--- src/model/resourceEvaluation.ts
+++ src/model/resourceEvaluation.ts
@@ -23,6 +23,42 @@ export function getResourceEvaluationDataQuery(userId: string) {
   return query;
 }
 
+export const gUserEvaluation = groq`
+  {
+    "resourceDetails": *[_id == $resourceId][0]{
+      "id": _id,
+      "title": coalesce(title, name),
+      "resourceUrl": coalesce(resourceUrl, appleUrl, spotifyUrl, playStoreUrl),
+    },
+    "evaluationDetails": *[_type == 'resourceEvaluation' 
+    && userId == $userId 
+    && resourceId == $resourceId][0]{
+      "id": _id,
+      userId,
+      resourceId,
+      rating
+    }
+  }
+`;
+
+export const zUserEvaluation = z.object({
+  resourceDetails: z
+    .object({
+      id: z.string(),
+      title: z.string(),
+      resourceUrl: z.string().url(),
+    })
+    .nullable(),
+  evaluationDetails: z
+    .object({
+      id: z.string(),
+      userId: z.string(),
+      resourceId: z.string(),
+      rating: z.number().nullable(),
+    })
+    .nullable(),
+});
+
 const zResourceEvaluationItem = z.object({
   id: z.string(),
   title: z.string(),
@@ -37,3 +73,4 @@ export const zResourceEvaluationData = z.record(
 
 export type ResourceEvaluationItem = z.infer<typeof zResourceEvaluationItem>;
 export type ResourceEvaluationData = z.infer<typeof zResourceEvaluationData>;
+export type UserEvaluation = z.infer<typeof zUserEvaluation>;
