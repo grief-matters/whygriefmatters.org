d54910a142f5cdf721c4b7c15bcc8c8377ca9e4d
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-11-19 16:07:06 -0800

    Added React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import clerk from "@clerk/astro";
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk(), tailwind({ applyBaseStyles: false })],
+  integrations: [clerk(), tailwind({ applyBaseStyles: false }), react()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
