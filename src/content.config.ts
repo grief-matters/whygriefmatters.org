b83d7b81906bf635f2e0fb74abedea83d951a834
Dan Chambers <danlechambre@icloud.com>
2026-01-10 12:20:00 +0000

    Update configs to work post package clean up

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,41 +1,118 @@
-import {
-  articles,
-  blogs,
-  booklets,
-  books,
-  courses,
-  forums,
-  memorials,
-  peerSupports,
-  podcastEpisodes,
-  podcasts,
-  stories,
-  supportGroups,
-  therapyResources,
-  videos,
-  webinars,
-} from "@content/baseInternetResource";
-import apps from "@content/apps";
-import categories from "@content/categories";
-import websites from "@content/websites";
+import { defineCollection } from "astro:content";
+
+import appsQuery from "@content/queries/apps.groq?raw";
+import categoriesQuery from "@content/queries/categories.groq?raw";
+import contentBlocksQuery from "@content/queries/contentBlocks.groq?raw";
+import contentGroupsQuery from "@content/queries/contentGroups.groq?raw";
+import imageCollectionsQuery from "@content/queries/imageCollections.groq?raw";
+import peopleQuery from "@content/queries/people.groq?raw";
+import personGroupsQuery from "@content/queries/personGroups.groq?raw";
+import populationsQuery from "@content/queries/populations.groq?raw";
+import websitesQuery from "@content/queries/websites.groq?raw";
+
+import zApp from "@content/model/app";
+import zCategory from "@content/model/category";
+import zContentBlock from "@content/model/contentBlock";
+import zContentGroup from "@content/model/contentGroup";
+import zImageCollection from "@content/model/imageCollection";
+import zPerson from "@content/model/person";
+import zPersonGroup from "@content/model/personGroup";
+import zPopulation from "@content/model/population";
+import zWebsite from "@content/model/website";
+
+import { loadSanityQuery } from "@content/loaders/sanityQueryLoader";
+import { getBasicInternetResourceCollectionDef } from "@content/utils";
 
 export const collections = {
-  apps,
-  articles,
-  blogs,
-  booklets,
-  books,
-  categories,
-  courses,
-  forums,
-  memorials,
-  peerSupports,
-  podcastEpisodes,
-  podcasts,
-  stories,
-  supportGroups,
-  therapyResources,
-  videos,
-  webinars,
-  websites,
+  // Internet Resources collections
+  articles: getBasicInternetResourceCollectionDef("article"),
+  blogs: getBasicInternetResourceCollectionDef("blog"),
+  books: getBasicInternetResourceCollectionDef("book"),
+  courses: getBasicInternetResourceCollectionDef("course"),
+  forums: getBasicInternetResourceCollectionDef("forum"),
+  peerSupports: getBasicInternetResourceCollectionDef("peerSupport"),
+  podcasts: getBasicInternetResourceCollectionDef("podcast"),
+  podcastEpisodes: getBasicInternetResourceCollectionDef("podcastEpisode"),
+  printedMaterial: getBasicInternetResourceCollectionDef("printedMaterial"),
+  stories: getBasicInternetResourceCollectionDef("story"),
+  supportGroups: getBasicInternetResourceCollectionDef("supportGroup"),
+  therapyResources: getBasicInternetResourceCollectionDef("therapyResource"),
+  videos: getBasicInternetResourceCollectionDef("video"),
+  webinars: getBasicInternetResourceCollectionDef("webinar"),
+  community: getBasicInternetResourceCollectionDef("community"),
+  memorials: getBasicInternetResourceCollectionDef("memorial"),
+  apps: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: appsQuery,
+        schema: zApp,
+      }),
+    schema: zApp,
+  }),
+  websites: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: websitesQuery,
+        schema: zWebsite,
+      }),
+    schema: zWebsite,
+  }),
+
+  // Other Collections
+  categories: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: categoriesQuery,
+        schema: zCategory,
+      }),
+    schema: zCategory,
+  }),
+  contentBlocks: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: contentBlocksQuery,
+        schema: zContentBlock,
+      }),
+    schema: zContentBlock,
+  }),
+  contentGroups: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: contentGroupsQuery,
+        schema: zContentGroup,
+      }),
+    schema: zContentGroup,
+  }),
+  populations: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: populationsQuery,
+        schema: zPopulation,
+      }),
+    schema: zPopulation,
+  }),
+  people: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: peopleQuery,
+        schema: zPerson,
+      }),
+    schema: zPerson,
+  }),
+  personGroups: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: personGroupsQuery,
+        schema: zPersonGroup,
+      }),
+    schema: zPersonGroup,
+  }),
+  imageCollections: defineCollection({
+    loader: async () =>
+      loadSanityQuery({
+        query: imageCollectionsQuery,
+        schema: zImageCollection,
+      }),
+    schema: zImageCollection,
+  }),
 };
