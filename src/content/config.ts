c528f1ebd5d82d3c1f2017453543fb6b74b71036
Dan Chambers <danlechambre@icloud.com>
2026-07-25 18:07:29 +0100

    implement triage nav

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+export default {
+  navigation: {
+    main: "main-navigation-v2",
+    triage: "triage-navigation",
+  },
+  contentGroup: {
+    home: "home",
+    search: "search",
+    supportingTheBereaved: "supporting-the-bereaved",
+  },
+} as const;
